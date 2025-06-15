4d45b32864a0bae4e0346434fc8c2202ca2a5a9d
elizraa <azrielwah@gmail.com>
2025-06-15 21:04:44 +0700

    feat: add level 3
    on progress adding fan and level 4

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -250.0
+const SPEED = 90.0
+const JUMP_VELOCITY = -240.0
 @onready var shoot_point = $ShootPoint
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var game_manager = %GameManager
